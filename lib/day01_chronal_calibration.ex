1e948bc3c437c470ce96b23504919d0350bb9223
Francois Perron <fperron@gmail.com>
2018-12-01 13:02:59 -0500

    Day 1 part 2 done

--- lib/day01_chronal_calibration.ex
+++ lib/day01_chronal_calibration.ex
@@ -1,15 +1,45 @@
 defmodule ChronalCalibration do
 
-  def from_file(file) do
+  def read(file) do
     File.stream!(file)
     |> Enum.join
-    |> ChronalCalibration.of
   end
 
-  def of(calibrations) do
+  def to_int(calibrations) do
     calibrations
     |> String.split("\n")
     |> Enum.map(&String.to_integer/1)
+  end
+
+  def apply(calibrations) do
+    calibrations
+    |> to_int
     |> Enum.sum
   end
+
+  def repeated(calibrations) do
+    calibrations
+    |> to_int
+    |> Stream.cycle
+    |> Enum.reduce_while([0], &find_repeated_frequency/2)
+  end
+
+  def find_repeated_frequency(calibration, frequencies) do
+    frequency = next_frequency(calibration, frequencies)
+
+    if is_repeated(frequency, frequencies) do
+      {:halt, frequency}
+    else
+      {:cont, [frequency | frequencies]}
+    end
+  end
+
+  def next_frequency(calibration, frequencies) do
+    List.first(frequencies) + calibration
+  end
+
+  def is_repeated(frequency, frequencies) do
+    Enum.member?(frequencies, frequency)
+  end
+
 end
--- test/day01_chronal_calibration_test.exs
+++ test/day01_chronal_calibration_test.exs
@@ -1,16 +1,41 @@
 defmodule ChronalCalibrationTest do
   use ExUnit.Case
 
-  test "calibrates one frequency" do
-    assert ChronalCalibration.of("+1") == 1
-    assert ChronalCalibration.of("-1") == -1
+  describe "part 1" do
+    test "calibrates using one calibration" do
+      assert ChronalCalibration.apply("+1") == 1
+      assert ChronalCalibration.apply("-1") == -1
+    end
+
+    test "calibrates with multiple calibrations" do
+      assert ChronalCalibration.apply("+1\n-4") == -3
+    end
+
+    test "solves it" do
+      assert ChronalCalibration.read("lib/day01_data") |> ChronalCalibration.apply === 459
+    end
   end
 
-  test "calibrates multiple frequencies" do
-    assert ChronalCalibration.of("+1\n-4") == -3
+  describe "part 2" do
+    test "checks if a frequency is repeated" do
+      assert ChronalCalibration.is_repeated(2, [2, 1]) === true
+      assert ChronalCalibration.is_repeated(3, [2, 1]) === false
+    end
+
+    test "calculates next frequency from last frequency" do
+      assert ChronalCalibration.next_frequency(3, [0]) === 3
+      assert ChronalCalibration.next_frequency(5, [4, 3, 0]) === 9
+    end
+
+    test "finds first repeated frequency" do
+      assert ChronalCalibration.repeated("+1\n-1") === 0
+      assert ChronalCalibration.repeated("3\n+3\n+4\n-2\n-4") === 10
+      assert ChronalCalibration.repeated("-6\n+3\n+8\n+5\n-6") === 5
+    end
+
+    test "solves it" do
+      assert ChronalCalibration.read("lib/day01_data") |> ChronalCalibration.repeated === 65474
+    end
   end
 
-  test "calibrates using part1 data" do
-    assert ChronalCalibration.from_file("lib/day01_data") === 459
-  end
 end
