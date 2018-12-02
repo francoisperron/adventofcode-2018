7cdfc3306f0098de1807da0816befa6c10d06661
Francois Perron <fperron@gmail.com>
2018-12-02 10:51:45 -0500

    Day 2 part 2

--- lib/day02_inventory_management_system.ex
+++ lib/day02_inventory_management_system.ex
@@ -7,7 +7,6 @@ defmodule InventoryManagementSystem do
     |> Enum.member?(count)
   end
 
-
   def checksum(ids) do
     double = ids
              |> Enum.map(&has_letters_appearing(2, &1))
@@ -19,4 +18,50 @@ defmodule InventoryManagementSystem do
 
     double * triple
   end
+
+  def closest_id(ids) do
+    Enum.reduce_while(
+      ids,
+      nil,
+      fn id1, acc ->
+        diff = find_id_with_one_diff(id1, ids)
+        if (elem(diff, 0) == 1) do
+          {:halt, elem(diff, 1)}
+        else
+          {:cont, nil}
+        end
+      end
+    )
+  end
+
+  def find_id_with_one_diff(id1, ids) do
+    Enum.reduce(
+      ids,
+      {0, ""},
+      fn id2, acc ->
+        diff = count_diff(id1, id2)
+        if (elem(diff, 0) == 1) do
+          diff
+        else
+          acc
+        end
+      end
+    )
+  end
+
+  def count_diff(s1, s2) do
+    diff = String.myers_difference(s1, s2)
+
+    count = diff
+            |> Keyword.get_values(:ins)
+            |> Enum.join()
+            |> String.length()
+
+    name = diff
+           |> Keyword.get_values(:eq)
+           |> Enum.join()
+
+    {count, name}
+  end
+
 end
--- test/day02_inventory_management_system_test.exs
+++ test/day02_inventory_management_system_test.exs
@@ -16,4 +16,30 @@ defmodule InventoryManagementSystemTest do
       assert checksum === 7221
     end
   end
+
+  describe "part 2" do
+    test "count diff between two ids" do
+      assert InventoryManagementSystem.count_diff("abcbe", "axcye") === {2, "ace" }
+      assert InventoryManagementSystem.count_diff("fghij", "fguij") === {1,  "fgij"}
+      assert InventoryManagementSystem.count_diff("abcde", "fghij") === {5,  ""}
+      assert InventoryManagementSystem.count_diff("nkuagflethzwsijxrqvymbdpoq", "wkucgflathznsijqrevymbdpoq") === {6,  "kugflthzsijqvymbdpoq"}
+    end
+
+    test "solves example data" do
+      id =
+        ["abcde","fghij","klmno","pqrst","fguij","axcye","wvxyz"]
+        |> InventoryManagementSystem.closest_id
+
+      assert id === "fgij"
+    end
+
+    test "solves it" do
+      id =
+        "day02_data"
+        |> DataFile.read_lines
+        |> InventoryManagementSystem.closest_id
+
+      assert id === "mkcdflathzwsvjxrevymbdpoq"
+    end
+  end
 end
