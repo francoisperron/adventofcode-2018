1920bee3c3fe5058ca9532938e8fe5008c1cfa83
Francois Perron <fperron@gmail.com>
2018-12-03 22:40:29 -0500

    Day 3 part 2

--- lib/day03/no_matter_how_you_slice_it.ex
+++ lib/day03/no_matter_how_you_slice_it.ex
@@ -4,8 +4,29 @@ defmodule NoMatterHowYouSliceIt do
     |> Enum.map(&Claim.parse/1)
     |> Enum.map(fn c -> c.area end)
     |> List.flatten()
-    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
-    |> Map.values()
-    |> Enum.count(fn o -> o > 1 end)
+    |> Enum.reduce(%{}, fn square, result -> Map.update(result, square, 1, &(&1 + 1)) end)
+    |> Enum.count(fn {_, count} -> count > 1 end)
+  end
+
+  def not_overlapping_claim_id(claims) do
+    overlapping =
+      claims
+      |> Enum.map(&Claim.parse/1)
+      |> Enum.map(fn c -> c.area end)
+      |> List.flatten()
+      |> Enum.reduce(%{}, fn square, result -> Map.update(result, square, 1, &(&1 + 1)) end)
+      |> Enum.filter(fn {_, count} -> count > 1 end)
+      |> Enum.map(&elem(&1, 0))
+      |> MapSet.new()
+
+    claims
+    |> Enum.map(&Claim.parse/1)
+    |> Enum.filter(&with_no_overlapping_square(&1, overlapping))
+    |> Enum.map(fn c -> c.id end)
+    |> List.first()
+  end
+
+  def with_no_overlapping_square(claim, overlapping) do
+    Enum.count(MapSet.intersection(MapSet.new(claim.area), overlapping)) == 0
   end
 end
--- lib/day03/no_matter_how_you_slice_it_test.exs
+++ lib/day03/no_matter_how_you_slice_it_test.exs
@@ -1,22 +1,38 @@
 defmodule NoMatterHowYouSliceItTest do
   use ExUnit.Case
 
-  test "parses a claim" do
-    assert Claim.parse("#123 @ 3,2: 2x2") === %Claim{
-             id: 123,
-             area: [{3, 2}, {3, 3}, {4, 2}, {4, 3}]
-           }
+  describe "part 1" do
+    test "parses a claim" do
+      assert Claim.parse("#123 @ 3,2: 2x2") === %Claim{
+               id: 123,
+               area: [{3, 2}, {3, 3}, {4, 2}, {4, 3}]
+             }
+    end
+
+    test "counts number of squares overlapping" do
+      data = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
+
+      assert NoMatterHowYouSliceIt.overlapping(data) === 4
+    end
+
+    test "solves it" do
+      data = DataFile.read_lines("day03/data")
+
+      assert NoMatterHowYouSliceIt.overlapping(data) === 105_231
+    end
   end
 
-  test "counts number of squares overlapping" do
-    data = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
+  describe "part2" do
+    test "finds claim not overlapping" do
+      data = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"]
 
-    assert NoMatterHowYouSliceIt.overlapping(data) === 4
-  end
+      assert NoMatterHowYouSliceIt.not_overlapping_claim_id(data) === 3
+    end
 
-  test "solves it" do
-    data = DataFile.read_lines("day03/data")
+    test "solves it" do
+      data = DataFile.read_lines("day03/data")
 
-    assert NoMatterHowYouSliceIt.overlapping(data) === 105_231
+      assert NoMatterHowYouSliceIt.not_overlapping_claim_id(data) === 164
+    end
   end
 end
