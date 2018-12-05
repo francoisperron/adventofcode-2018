60858aaf4b46574393cb5ca491d8164925614993
Francois Perron <fperron@gmail.com>
2018-12-04 22:31:13 -0500

    Day 4 part 2
    So ugly

--- lib/day04/repose_record.ex
+++ lib/day04/repose_record.ex
@@ -76,11 +76,72 @@ defmodule ReposeRecord do
                    end
                  )
 
-      records.minutes[das_guard]
-      |> Enum.reduce(%{}, fn square, result -> Map.update(result, square, 1, &(&1 + 1)) end)
-      |> Enum.sort_by(&(elem(&1, 1)))
-      |> Enum.reverse()
-      |> List.first()
-      |> elem(0)
+    records.minutes[das_guard]
+    |> Enum.reduce(%{}, fn minute, result -> Map.update(result, minute, 1, &(&1 + 1)) end)
+    |> Enum.sort_by(&(elem(&1, 1)))
+    |> Enum.reverse()
+    |> List.first()
+    |> elem(0)
+  end
+
+
+  def find_guard_asleep_the_most_on_the_same_minute(data) do
+    records = data
+              |> order
+              |> Enum.reduce(
+                   %{guard: 0, minutes: %{}},
+                   fn record, acc ->
+                     guard =
+                       case String.contains?(record.note, "Guard") do
+                         true ->
+                           String.split(record.note, ~r/[+x,# ]/, trim: true)
+                           |> Enum.fetch(1)
+                           |> elem(1)
+                         _ -> acc.guard
+                       end
+
+                     asleep =
+                       case String.contains?(record.note, "falls") do
+                         true -> record.timestamp
+                         _ -> nil
+                       end
+
+                     minutes =
+                       case String.contains?(record.note, "wakes") do
+                         true ->
+                           Range.new(record.timestamp.minute - 1, acc.asleep.minute)
+                           |> Enum.to_list()
+                         _ -> []
+                       end
+
+                     %{guard: guard, asleep: asleep, minutes: Map.update(acc.minutes, guard, [0], &(&1 ++ minutes))}
+                   end
+                 )
+
+    out = records.minutes
+          |> Enum.map(
+               fn {k, v} ->
+                 Enum.reduce(
+                   v,
+                   %{guard: k, minutes: %{}},
+                   fn minute, result ->
+                     %{guard: result.guard, minutes: Map.update(result.minutes, minute, 0, &(&1 + 1))}
+                   end
+                 )
+               end
+             )
+          |> Enum.map(
+               fn v ->
+                 minute =
+                   v.minutes
+                   |> Enum.sort_by(&(elem(&1, 1)))
+                   |> List.last()
+                 %{guard: v.guard, minute: elem(minute, 0), count: elem(minute, 1)}
+               end
+             )
+          |> Enum.sort_by(fn v -> v.count end)
+          |> List.last()
+
+    String.to_integer(out.guard) * out.minute
   end
 end
--- lib/day04/repose_record_test.exs
+++ lib/day04/repose_record_test.exs
@@ -23,41 +23,55 @@ defmodule ReposeRecordTest do
     ]
   end
 
-  test "parses a record" do
-    assert MyRecord.parse("[1518-03-30 00:57] wakes up") ===
-             %MyRecord{timestamp: ~N[1518-03-30 00:57:00], note: "wakes up"}
+  describe "part 1" do
+    test "parses a record" do
+      assert MyRecord.parse("[1518-03-30 00:57] wakes up") ===
+               %MyRecord{timestamp: ~N[1518-03-30 00:57:00], note: "wakes up"}
+    end
+
+    test "orders records by date" do
+      records = [
+        "[1518-04-15 23:56] Guard #2213 begins shift",
+        "[1518-10-31 00:36] wakes up",
+        "[1518-03-30 00:57] wakes up"
+      ]
+
+      assert ReposeRecord.order(records) ===
+               [
+                 %MyRecord{timestamp: ~N[1518-03-30 00:57:00], note: "wakes up"},
+                 %MyRecord{timestamp: ~N[1518-04-15 23:56:00], note: "Guard #2213 begins shift"},
+                 %MyRecord{timestamp: ~N[1518-10-31 00:36:00], note: "wakes up"}
+               ]
+    end
+
+
+    test "finds guard asleep the most" do
+      assert ReposeRecord.find_guard_asleep_the_most(test_data()) === 10
+    end
+
+    test "finds minute most asleep for a guard" do
+      assert ReposeRecord.find_minute_most_asleep_for_guard("10", test_data()) === 24
+    end
+
+    test "solves it" do
+      data = DataFile.read_lines("day04/data")
+
+      assert ReposeRecord.find_guard_asleep_the_most(data) === 1777
+      assert ReposeRecord.find_minute_most_asleep_for_guard("1777", data) === 48
+      assert 1777 * 48 === 85296
+    end
   end
 
-  test "orders records by date" do
-    records = [
-      "[1518-04-15 23:56] Guard #2213 begins shift",
-      "[1518-10-31 00:36] wakes up",
-      "[1518-03-30 00:57] wakes up"
-    ]
+  describe "part 2" do
+    test "finds guard asleep the most on the same minute" do
+      assert ReposeRecord.find_guard_asleep_the_most_on_the_same_minute(test_data()) === 4455
+    end
 
-    assert ReposeRecord.order(records) ===
-             [
-               %MyRecord{timestamp: ~N[1518-03-30 00:57:00], note: "wakes up"},
-               %MyRecord{timestamp: ~N[1518-04-15 23:56:00], note: "Guard #2213 begins shift"},
-               %MyRecord{timestamp: ~N[1518-10-31 00:36:00], note: "wakes up"}
-             ]
-  end
+    test "solves it" do
+      data = DataFile.read_lines("day04/data")
 
-
-  test "finds guard asleep the most" do
-    assert ReposeRecord.find_guard_asleep_the_most(test_data()) === 10
-  end
-
-  test "finds minute most asleep for a guard" do
-    assert ReposeRecord.find_minute_most_asleep_for_guard("10", test_data()) === 24
-  end
-
-  test "solves it" do
-    data = DataFile.read_lines("day04/data")
-
-    assert ReposeRecord.find_guard_asleep_the_most(data) === 1777
-    assert ReposeRecord.find_minute_most_asleep_for_guard("1777", data) === 48
-    assert 1777 * 48 === 85296
+      assert ReposeRecord.find_guard_asleep_the_most_on_the_same_minute(data) === 58559
+    end
   end
 
 end
